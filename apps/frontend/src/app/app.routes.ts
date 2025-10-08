5171b8382dc2c11e269594002b3694841e8b7d0a
Jazmin Bascuñan <jazbascunan01@gmail.com>
2025-10-08 20:51:53 -0300

    Se añade componente contacto y about

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' }
 ];
