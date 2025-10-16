9b5cfe447711494b78ed33ac8633e7648833168d
Jazmin Bascuñan <jazbascunan01@gmail.com>
2025-10-16 20:05:42 -0300

    Feat: Add user authentication check to product card component for conditional rendering of add to cart button

--- apps/frontend/src/app/components/product-card/product-card.component.ts
+++ apps/frontend/src/app/components/product-card/product-card.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../core/models/product.model';
 import { RouterLink } from '@angular/router';
-import {CommonModule, CurrencyPipe} from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { CartService } from '../../core/state/cart.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { Observable } from 'rxjs';
+import { User } from '../../core/models/user.model';
 
 @Component({
   selector: 'app-product-card',
@@ -13,7 +16,14 @@ import { CartService } from '../../core/state/cart.service';
 })
 export class ProductCardComponent {
   @Input() product!: Product;
-  constructor(private cartService: CartService) {}
+  user$: Observable<User | null>;
+
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService
+  ) {
+    this.user$ = this.authService.user$;
+  }
 
   onAddToCart(): void {
     this.cartService.addProduct(this.product);
